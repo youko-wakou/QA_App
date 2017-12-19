ca54aec68611634ac83800d55953df8e6cea8ae8
youko-wakou <appu2pudding@gmail.com>
2017-12-19 23:29:26 +0900

    8.5 MainActivity.javaの修正

--- app/src/main/java/jp/techacademy/wakou/youko/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/wakou/youko/qa_app/MainActivity.java
@@ -1,24 +1,123 @@
 package jp.techacademy.wakou.youko.qa_app;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+        import android.os.Bundle;
+        import android.support.design.widget.FloatingActionButton;
+        import android.support.design.widget.NavigationView;
+        import android.support.design.widget.Snackbar;
+        import android.support.v4.view.GravityCompat;
+        import android.support.v4.widget.DrawerLayout;
+        import android.support.v7.app.ActionBarDrawerToggle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
+        import android.util.Base64;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.ListView;
+
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+        import com.google.firebase.database.ChildEventListener;
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
+        import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+        import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private int mGenre = 0;
+
+    private DatabaseReference mDatabaseReference;
+    private DatabaseReference mGenreRef;
+    private ListView mListView;
+    private ArrayList<Question>mQuestionArrayList;
+    private QuestionsListAdapter mAdapter;
+
+    private ChildEventListener mEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                HashMap map = (HashMap)dataSnapshot.getValue();
+                String title = (String) map.get("title");
+                String body = (String)map.get("body");
+                String name = (String)map.get("name");
+                String uid = (String)map.get("uid");
+                String imageString = (String)map.get("image");
+                byte[]bytes;
+
+                if(imageString != null){
+                    bytes = Base64.decode(imageString,Base64.DEFAULT);
+                }else{
+                    bytes = new byte[0];
+                }
+
+                ArrayList<Answer>answerArrayList = new ArrayList<Answer>();
+                HashMap answerMap = (HashMap)map.get("answers");
+                if(answerMap != null){
+                    for(Object key:answerMap.keySet()){
+                        HashMap temp = (HashMap)answerMap.get((String)key);
+                        String answerBody = (String)temp.get("body");
+                        String answerName = (String)temp.get("name");
+
+                        String answerUid = (String)temp.get("uid");
+                        Answer answer = new Answer(answerBody,answerName,answerUid,(String)key);
+                        answerArrayList.add(answer);
+                    }
+                }
+
+                Question question = new Question(title,body,name,uid,dataSnapshot.getKey(),mGenre,bytes,answerArrayList);
+                mQuestionArrayList.add(question);
+                mAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onChildChanged(DataSnapshotdataSnapshot,String s){
+                HashMap map = (HashMap)dataSnapshot.getValue();
+
+                for(Question question:mQuestionArrayList){
+                    if(dataSnapshot.getKey().equals(question.getQuestionUid())){
+                        question.getAnswers().clear();
+                        HashMap answerMap = (HashMap)map.get("answers");
+                        if(answerMap != null){
+                            for(Object key: answerMap.KeySet()){
+                                HashMap temp = (HashMap)answerMap.get(String)key);
+                                String answerBody = (String)temp.get("body");
+                                String answerName = (String)temp.get("name");
+                                String answerUid = (String)temp.get("uid");
+
+                                Answer answer = new Answer(answerBody,answerName,answerUid,(String)key);
+                                question.getAnswers().add(answer);
+                            }
+                        }
+                        mAdapter.notifyDataSetChanged();
+                    }
+            }
+
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,9 +172,26 @@ public class MainActivity extends AppCompatActivity {
 
                 DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
                 drawer.closeDrawer(GravityCompat.START);
+
+                mQuestionArrayList.clear();
+                mAdapter.setQuestionArrayList(mQuestionArrayList);
+                mListView.setAdapter(mAdapter);
+
+                if(mGenreRef != null){
+                    mGenreRef.removeEventListener(mEventListener);
+                }
+                mGenreRef = mDatabaseReference.child(Const.ContentsPATH).child(String.valueOf(mGenre));
+                mGenreRef.addChildEventListener(mEventListener);
                 return true;
             }
         });
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+        mListView = (ListView)findViewById(R.id.listView);
+        mAdapter = new QuestionsListAdapter(this);
+        mQuestionArrayList = new ArrayList<Question>();
+        mAdapter.notifyDataSetChanged();
     }
 
     @Override

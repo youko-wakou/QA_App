b62a15ec6315371c62b96cb010f6eaf2661b3828
youko-wakou <appu2pudding@gmail.com>
2017-12-19 17:26:32 +0900

    6.3 MainActivity.javaの修正

--- app/src/main/java/jp/techacademy/wakou/youko/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/wakou/youko/qa_app/MainActivity.java
@@ -84,6 +84,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(getApplicationContext(),SettingActivity.class);
+            startActivity(intent);
             return true;
         }
 

46c24ea63f3f3d9971a09c63a898c1ab77e24ba7
youko-wakou <appu2pudding@gmail.com>
2017-12-21 21:15:16 +0900

    「お気に入り」ボタンを表示

--- app/src/main/java/jp/techacademy/wakou/youko/qa_app/QuestionListActivity.java
+++ app/src/main/java/jp/techacademy/wakou/youko/qa_app/QuestionListActivity.java
@@ -1,5 +1,6 @@
 package jp.techacademy.wakou.youko.qa_app;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -15,13 +16,10 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/**
- * Created by appu2 on 2017/12/21.
- */
-
 public class QuestionListActivity extends AppCompatActivity {
     private Button mfavo_b;
-    public void onCreate(Bundle savedInstanceState){
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mfavo_b = findViewById(R.id.favobt);
         mfavo_b.setOnClickListener(new View.OnClickListener() {
@@ -32,9 +30,8 @@ public class QuestionListActivity extends AppCompatActivity {
     }
     public class QuestionsListAdapter extends BaseAdapter {
         private LayoutInflater mLayoutInflater = null;
-        private ArrayList<Question> mQuestionArrayList;
-
-        @Override
+        private ArrayList<Question>mQuestionArrayList;
+         @Override
         public int getCount(){
             return mQuestionArrayList.size();
         }
@@ -47,30 +44,30 @@ public class QuestionListActivity extends AppCompatActivity {
             return position;
         }
         @Override
-        public View getView(int position,View convertView,ViewGroup parent) {
-            if (convertView == null) {
-                convertView = mLayoutInflater.inflate(R.layout.list_questions, parent, false);
+        public View getView(int position, View convertView, ViewGroup parent){
+            if(convertView == null){
+                convertView = mLayoutInflater.inflate(R.layout.list_questions,parent,false);
             }
             TextView titleText = (TextView) convertView.findViewById(R.id.titleTextView);
             titleText.setText(mQuestionArrayList.get(position).getTitle());
 
-            TextView nameText = (TextView) convertView.findViewById(R.id.nameTextView);
+            TextView nameText = (TextView)convertView.findViewById(R.id.nameTextView);
             nameText.setText(mQuestionArrayList.get(position).getName());
 
             TextView resText = (TextView) convertView.findViewById(R.id.resTextView);
             int resNum = mQuestionArrayList.get(position).getAnswers().size();
             resText.setText(String.valueOf(resNum));
 
-            byte[] bytes = mQuestionArrayList.get(position).getImageBytes();
-            if (bytes.length != 0) {
-                Bitmap image = BitmapFactory.decodeByteArray(bytes, 0, bytes.length).copy(Bitmap.Config.ARGB_8888, true);
-                ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+            byte[]bytes = mQuestionArrayList.get(position).getImageBytes();
+            if(bytes.length != 0){
+                Bitmap image = BitmapFactory.decodeByteArray(bytes,0,bytes.length).copy(Bitmap.Config.ARGB_8888,true);
+                ImageView imageView = (ImageView)convertView.findViewById(R.id.imageView);
                 imageView.setImageBitmap(image);
             }
             return convertView;
         }
-
-    }
-
+     }
 }
+
+
 
